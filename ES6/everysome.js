06c28895fca8f092702b9b3a22af113a119e94cf
ssongalice <songyijuni@naver.com>
2018-11-22 17:22:55 +0900

    080일 커밋

---
+++ ES6/everysome.js
@@ -0,0 +1,22 @@
+// 1. Finding Submitted Users
+var users = [
+  { id: 21, hasSubmitted: true },
+  { id: 62, hasSubmitted: false },
+  { id: 4, hasSubmitted: true }
+];
+
+var hasSubmitted = users.every(function(user) {
+    return user.hasSubmitted === true;
+});
+
+
+// 2. In progress Network Requests
+var requests = [
+  { url: '/photos', status: 'complete' },
+  { url: '/albums', status: 'pending' },
+  { url: '/users', status: 'failed' }
+];
+
+var inProgress = requests.some(function(request) {
+    return request.status === 'pending';
+});
