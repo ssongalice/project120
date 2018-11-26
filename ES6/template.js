711d1f830decaa6b943e318dd740713264c8c54e
ssongalice <songyijuni@naver.com>
2018-11-26 18:09:03 +0900

    084일 커밋

---
+++ ES6/template.js
@@ -0,0 +1,10 @@
+// 1. Template Strings in Practice
+
+function doubleMessage(number) {
+  return `Your number doubled is ${ (2 * number) }`;
+}
+
+// 2. Name Helpers
+function fullName(firstName, lastName) {
+  return `${firstName} ${lastName}`
+}
